6c6011b895accc9c952db8b5ce1cf61b9af0ee2b
akashSharma135 <imakashsharma135@gmail.com>
2021-09-03 18:51:15 +0530

    second-commit: fixed some bugs in the taskAPI

--- app/api/user.py
+++ app/api/user.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
 from .auth import auth
+from .task import task
 
 user = Blueprint('user', __name__)
 
 # user auth route blueprint registration
 user.register_blueprint(auth, url_prefix='/auth')
+
+user.register_blueprint(task, url_prefix='/task')
